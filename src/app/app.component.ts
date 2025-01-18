3e4f15aadfff5b92096a9aea3dd7c6b6bca0d0a2
Matheus Ribeiro <omatheusribeiro@outlook.com>
2025-01-17 23:01:45 -0300

    Simultaneous translation feature completed

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,19 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { TranslateModule, TranslateService } from '@ngx-translate/core';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, TranslateModule],
+  standalone: true,
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
   title = 'unify-front';
+
+  constructor(public translate: TranslateService) {
+    const browserLang = navigator.language.split('-')[0]; // Detecta o idioma do navegador
+    translate.use(browserLang.match(/en|pt/) ? browserLang : 'en'); // Usa 'en' como padrão
+  }  
 }
--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,23 @@
 import { bootstrapApplication } from '@angular/platform-browser';
-import { appConfig } from './app/app.config';
 import { AppComponent } from './app/app.component';
+import { provideRouter } from '@angular/router';
+import { importProvidersFrom } from '@angular/core';
+import { HttpClient, HttpClientModule } from '@angular/common/http';
+import { TranslateModule, TranslateLoader } from '@ngx-translate/core';
+import { HttpLoaderFactory } from './app/app-translate.loader';
 
-bootstrapApplication(AppComponent, appConfig)
-  .catch((err) => console.error(err));
+bootstrapApplication(AppComponent, {
+  providers: [
+    provideRouter([]),
+    importProvidersFrom(
+      HttpClientModule,
+      TranslateModule.forRoot({
+        loader: {
+          provide: TranslateLoader,
+          useFactory: HttpLoaderFactory,
+          deps: [HttpClient],
+        },
+      })
+    )
+  ],
+}).catch(err => console.error(err));
